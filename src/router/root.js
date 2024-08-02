9250866ef4b63ac56ab24e97201ad20189965200
jykim <nyhyhy18@gmail.com>
2024-08-02 10:18:15 +0900

    테스트 연습 페이지 추가 함

---
+++ src/router/root.js
@@ -0,0 +1,30 @@
+import { Suspense, lazy } from "react";
+import Mainpage from "../pages/MainPage";
+
+const { createBrowserRouter } = require("react-router-dom");
+const Loading = <div>Loading...</div>
+const Main = lazy(() => import("../pages/MainPage"))
+const About = lazy(() => import("../pages/AboutPage"))
+const Test = lazy(() => import("../pages/TestPage"))
+const Test2 = lazy(() => import("../pages/TestPage2"))
+
+const root = createBrowserRouter([
+    {
+        path: "",
+        element: <Suspense fallback={Loading}><Main/></Suspense>
+    },
+    {
+        path: "about",
+        element: <Suspense fallback={Loading}><About/></Suspense>
+    },
+    {
+        path: "test",
+        element: <Suspense fallback={Loading}><Test/></Suspense>
+    },
+    {
+        path: "test2",
+        element: <Suspense fallback={Loading}><Test2/></Suspense>
+    }
+])
+
+export default root;
