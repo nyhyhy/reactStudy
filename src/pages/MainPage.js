30457c05919489baa8f938c5677227b8cc4ba6fb
jykim <nyhyhy18@gmail.com>
2024-08-05 09:57:14 +0900

    link 추가

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,7 +1,10 @@
+import { Link } from "react-router-dom";
+
 const Mainpage = () => {
     return (
         <div className=" text-3xl">
             <div>Main page</div>
+            <div><Link to={'/about'}>Go Main Page!</Link></div>
         </div>
     );
 }
