36538344855ded720b2f06a37f76096caa2c13aa
jykim <nyhyhy18@gmail.com>
2024-08-14 16:56:41 +0900

    tailwind-css 설정 추가, childern 설정 추가

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,11 +1,19 @@
 import { Link } from "react-router-dom";
+import BasicLayout from "../layouts/BasicLayout";
 
 const Mainpage = () => {
     return (
-        <div className=" text-3xl">
-            <div>Main page</div>
-            <div><Link to={'/about'}>Go About Page!</Link></div>
-        </div>
+        // <div className=" text-3xl">
+        //     <div>Main page</div>
+        //     <div><Link to={'/about'}>Go About Page!</Link></div>
+        // </div>
+        <BasicLayout>
+            <div className=" text-3xl ">Main page</div>
+
+            <div className="text-4xl font-extrabold m-2 p-3 bg-green-500">
+      Hello World
+    </div>
+        </BasicLayout>
     );
 }
 
